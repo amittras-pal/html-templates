d059bbc12ff33df268372ebfc29eea47ebb553c8
amittras-pal <pal.amittras@gmail.com>
2022-04-15 18:20:47 +0530

    New Data

---
+++ digital-ninjas/scritps/scripts.js
@@ -0,0 +1,16 @@
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl, {
+    html: true,
+    customClass: "dn-popper shadow",
+  });
+});
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
